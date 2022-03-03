640a94681a9971cae07d2132e667383c87568685
Pho Hale <CommanderPho@users.noreply.github.com>
2022-03-02 19:05:50 -0500

    added PhoUIContainer

---
+++ src/pyphocorehelpers/gui/PhoUIContainer.py
@@ -0,0 +1,6 @@
+
+
+class PhoUIContainer(object):
+	"""A simple extensable container that allows setting properties dynamically to hold UI elements or other important data."""
+	def __init__(self):
+		super(PhoUIContainer, self).__init__()

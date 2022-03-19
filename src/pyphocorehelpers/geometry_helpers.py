9cae673643ee9904240a314f185c50d1c63f1fd3
Pho Hale <CommanderPho@users.noreply.github.com>
2022-03-18 20:39:27 -0400

    created find_ranges_in_window function to find epochs within an active window

--- src/pyphocorehelpers/geometry_helpers.py
+++ src/pyphocorehelpers/geometry_helpers.py
@@ -131,3 +131,67 @@ def compute_data_aspect_ratio(xbin, ybin, sorted_inputs=True):
     aspect_ratio = width / height
     return aspect_ratio, Width_Height_Tuple(width, height)
     
+
+
+def find_ranges_in_window(epoch_starts, epoch_ends, active_window):
+    """ finds the epochs that lie either wholly or partially within the active_Window.
+    This can be used for example to find the epochs that fall within the visualization window to know which rectangles to draw.
+    
+    Inputs:
+        epoch_starts: an array of size {N,}
+        epoch_ends: an array of size {N,}
+        active_window: a tuple or list containing 2 elements representing the (win_start, win_end) times of the current window
+        
+    Returns:
+        is_range_in_window: a boolean array of size {N,} that indicates whether the epoch is (either partially or fully) within the active window.
+        included_epoch_indicies: an array of size {F,} containing the index of the original input epochs
+        included_epoch_starts: an array of size {F,} containing the (potentially modified if truncated) start times for each epoch in the window
+        included_epoch_ends: an array of size {F,} containing the (potentially modified if truncated) stop times for each epoch in the window
+        included_epoch_is_truncated: an array of size {F,} containing a boolean value indicating whether that epoch was truncated by the edges of the window
+
+    Usage:
+        is_range_in_window, included_epoch_indicies, included_epoch_starts, included_epoch_ends, included_epoch_is_truncated = find_ranges_in_window(curr_sess.pbe.starts, curr_sess.pbe.stops, (598.65, 820.0))
+        print(f'included_epoch_indicies: {np.shape(included_epoch_indicies)}')
+        print(f'included_epoch_starts: {np.shape(included_epoch_starts)}')
+        # build a dataframe to preview output
+        pd.DataFrame({'included_epoch_indicies':included_epoch_indicies, 'included_epoch_starts':included_epoch_starts, 'included_epoch_ends':included_epoch_ends, 'included_epoch_is_truncated': included_epoch_is_truncated})
+    
+    
+    """
+    active_window_start, active_window_end = active_window
+    # found_start_indicies = np.searchsorted(times_arr, start_stop_times_arr[:,0], side='left')
+    # found_end_indicies = np.searchsorted(times_arr, start_stop_times_arr[:,1], side='right') # find the end of the range
+    
+    # find all epochs that end before the start of the window to exclude them.
+    # found_excluded_early_windows = (epoch_ends >= active_window_start)
+        
+    # find all epochs that start after the end of the window to exclude them.
+    # found_excluded_late_windows = (epoch_starts >= active_window_end)
+    
+    # Combined for efficiency
+    excluded_windows = np.logical_or((epoch_ends <= active_window_start), (epoch_starts >= active_window_end))
+    is_range_in_window = np.logical_not(excluded_windows) # if True, range is at least partially in the window
+    
+    included_epoch_indicies = np.squeeze(np.where(is_range_in_window)) # np.where returns (1, 39) for some reason instead of (39,)
+    included_epoch_starts = epoch_starts[included_epoch_indicies]
+    included_epoch_ends = epoch_ends[included_epoch_indicies]
+    included_epoch_is_truncated = np.full_like(included_epoch_starts, False)
+    
+    # TODO: return if empty
+    if len(included_epoch_starts) > 0:
+        # Left truncation occurs when (included_epoch_starts[0] < active_window_start)
+        if (included_epoch_starts[0] < active_window_start):
+            # left truncation
+            included_epoch_is_truncated[0] = True
+            included_epoch_starts[0] = active_window_start
+
+        # Right truncation occurs when (included_epoch_ends[-1] > active_window_end)
+        if (included_epoch_ends[-1] > active_window_end):
+            # right truncation
+            included_epoch_is_truncated[-1] = True
+            included_epoch_starts[-1] = active_window_end
+            
+    return is_range_in_window, included_epoch_indicies, included_epoch_starts, included_epoch_ends, included_epoch_is_truncated
+    
+    
+    

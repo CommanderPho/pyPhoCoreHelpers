f2cb10565e1a062f2b27cce45c172e65d1f2ca4f
Pho Hale <CommanderPho@users.noreply.github.com>
2022-04-20 13:46:54 -0400

    broke out set_pho_preferences(...) functions into separate calls so not all of them have to be called at once

--- src/pyphocorehelpers/preferences_helpers.py
+++ src/pyphocorehelpers/preferences_helpers.py
@@ -1,5 +1,4 @@
-import numpy as np
-import pandas as pd
+
 
 
 def set_pho_preferences():
@@ -7,6 +6,14 @@ def set_pho_preferences():
     
     Includes increasing the number of rows/columns displayed for Pandas dataframes, and setting the numpy print options to be full-width for the jupyter notebook.
     """
+    set_pho_numpy_display_preferences()
+    set_pho_pandas_display_preferences()
+    
+
+
+    
+def set_pho_pandas_display_preferences():
+    import pandas as pd
     ## Pandas display options
     # pd.set_option('display.max_columns', None)  # or 1000
     # pd.set_option('display.max_rows', None)  # or 1000
@@ -18,10 +25,17 @@ def set_pho_preferences():
     # pd.set_option('display.width', 1000)
     pd.set_option('display.min_rows', 30)
     pd.set_option('display.max_rows', 50)
-
+    
+    
+    
+def set_pho_numpy_display_preferences():
+    import numpy as np
     ## Numpy display options:
     # Set up numpy print options to only wrap at window width:
     np.set_printoptions(edgeitems=30, linewidth=100000, formatter=dict(float=lambda x: "%g" % x))
     # np.set_printoptions(edgeitems=3, linewidth=4096, formatter=dict(float=lambda x: "%.3g" % x))
     np.core.arrayprint._line_width = 144
     
+
+# def set_pho_jupyter_lab_display_preferences():
+#     %config Completer.use_jedi = False

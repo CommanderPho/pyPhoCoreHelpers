8f669fae9025ddfeaaf3534eef76c27304b1693b
Pho Hale <PhoHale@gmail.com>
2023-05-30 21:49:16 -0400

    cleanup

--- src/pyphocorehelpers/DataStructure/RenderPlots/MatplotLibRenderPlots.py
+++ src/pyphocorehelpers/DataStructure/RenderPlots/MatplotLibRenderPlots.py
@@ -6,7 +6,18 @@ class MatplotlibRenderPlots(RenderPlots):
 	Usage:
 		from pyphocorehelpers.DataStructure.RenderPlots.MatplotLibRenderPlots import MatplotlibRenderPlots
 	
+	2023-05-30 - Updated to replace subplots.
+	
 	"""
 	def __init__(self, name='MatplotlibRenderPlots', figures=[], axes=[], **kwargs):
 		super(MatplotlibRenderPlots, self).__init__(name, figures = figures, axes=axes, **kwargs)
 		
+	@classmethod
+	def init_subplots(cls, *args, **kwargs):
+		""" wraps the matplotlib.plt.subplots(...) command to initialize a new object with custom subplots. 
+		2023-05-30 - Should this be here ideally? Is there a better class? I remember a MATPLOTLIB
+		"""
+		import matplotlib.pyplot as plt
+		fig, axes = plt.subplots(*args, **kwargs)
+		# check for scalar axes and wrap it in a tuple if needed before setting self.
+		return cls(figures=[fig], axes=axes)

e0e4a6631c9184ab80bd7b8f504ec5cb15499224
Pho Hale <PhoHale@gmail.com>
2023-06-06 20:09:30 -0400

    added very useful `discover_data_files(...)` function

--- src/pyphocorehelpers/Filesystem/path_helpers.py
+++ src/pyphocorehelpers/Filesystem/path_helpers.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from typing import List
 import shutil # for _backup_extant_file(...)
 from datetime import datetime
+from pyphocorehelpers.function_helpers import function_attributes
 
 def build_unique_filename(file_to_save_path, additional_postfix_extension=None):
     """ builds a unique filename for the file to be saved at file_to_save_path.
@@ -26,8 +27,6 @@ def build_unique_filename(file_to_save_path, additional_postfix_extension=None):
     unique_save_path = parent_path.joinpath(unique_file_name)
     # print(f"'{file_to_save_path}' backing up -> to_file: '{unique_save_path}'")
     return unique_save_path, unique_file_name
-
-
 
 def backup_extant_file(file_to_backup_path, MAX_BACKUP_AMOUNT=2):
     """creates a backup of an existing file that would otherwise be overwritten
@@ -74,7 +73,6 @@ def backup_extant_file(file_to_backup_path, MAX_BACKUP_AMOUNT=2):
     # dest.write_bytes(src.read_bytes()) #for binary files
     # dest.write_text(src.read_text()) #for text files
 
-
 def find_first_extant_path(path_list: List[Path]) -> Path:
     """Returns the first path in the list that exists.
     from pyphocorehelpers.Filesystem.path_helpers import find_first_extant_path
@@ -94,3 +92,47 @@ def find_first_extant_path(path_list: List[Path]) -> Path:
     raise FileNotFoundError(f"Could not find any of the paths in the list: {path_list}")
 
 
+@function_attributes(short_name=None, tags=['filesystem','find','search','discover','data','files'], input_requires=[], output_provides=[], uses=[], used_by=[], creation_date='2023-06-06 20:08', related_items=['print_data_files_list_as_array'])
+def discover_data_files(basedir: Path, glob_pattern='*.mat', recursive=True):
+    """ By default it attempts to find the all *.mat files in the root of this basedir
+    Example:
+        basedir: Path(r'~/data/Bapun/Day5TwoNovel')
+        session_name: 'RatS-Day5TwoNovel-2020-12-04_07-55-09'
+
+    Example 2:
+        from pyphocorehelpers.Filesystem.path_helpers import discover_data_files
+        
+        search_parent_path = Path(r'W:\\Data\\Kdiba')
+        found_autoversioned_session_pickle_files = discover_data_files(search_parent_path, glob_pattern='*-loadedSessPickle.pkl', recursive=True)
+        found_global_computation_results_files = discover_data_files(search_parent_path, glob_pattern='output/*.pkl', recursive=True)
+
+        found_files = found_global_computation_results_files + found_autoversioned_session_pickle_files
+        found_files
+
+    """
+    if isinstance(basedir, str):
+        basedir = Path(basedir) # convert to Path object if not already one.
+    if recursive:
+        glob_pattern = f"**/{glob_pattern}"
+    else:
+        glob_pattern = f"{glob_pattern}"
+    found_files = sorted(basedir.glob(glob_pattern))
+    return found_files # 'RatS-Day5TwoNovel-2020-12-04_07-55-09'
+
+
+
+@function_attributes(short_name=None, tags=['filesystem','find','search','discover','data','files'], input_requires=[], output_provides=[], uses=[], used_by=[], creation_date='2023-06-06 20:09', related_items=['discover_data_files'])
+def print_data_files_list_as_array(filenames_list):
+    """ tries to print the output list of data files from `discover_data_files` as a printable array. 
+    
+    Usage:
+        from pyphocorehelpers.Filesystem.path_helpers import discover_data_files
+        
+        print_data_files_list_as_array(found_files)
+    """
+    # print(f'filenames_list: {filenames_list}')
+    # filenames_list_str = ',\n'.join([str(a_path) for a_path in filenames_list])
+    filenames_list_str = ',\n'.join([f'r"{str(a_path)}"' for a_path in filenames_list])
+    print(f'filenames_list_str: [{filenames_list_str}]')
+    # for a_path in filenames_list:
+    #     print(f'{str(a_path)}')

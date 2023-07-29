799931ec575a60c5949f20964a81bdfd012bca59
Pho Hale <PhoHale@gmail.com>
2023-07-29 07:42:38 -0400

    added project_name, details_print to build_code_index

--- src/pyphocorehelpers/Filesystem/generate_code_index.py
+++ src/pyphocorehelpers/Filesystem/generate_code_index.py
@@ -1,9 +1,10 @@
 import subprocess
 import pathlib
+from typing import Optional
 
 from pyphocorehelpers.Filesystem.source_code_helpers import find_py_files
 
-def build_code_index(project_path, exclude_dirs=[]):
+def build_code_index(project_path, exclude_dirs=[], project_name:Optional[str]=None, details_print=True):
     """ 2023-05-11 - Tries to build a CodeQuery code index. Not quite working.
     from pyphocorehelpers.Filesystem.generate_code_index import build_code_index
     Original Bash Commands:
@@ -42,30 +43,50 @@ def build_code_index(project_path, exclude_dirs=[]):
     Example:
         project_path = "C:/Users/pho/repos/Spike3DWorkEnv/pyPhoPlaceCellAnalysis/src"
         exclude_dirs = ["pyphoplacecellanalysis/External"]
+        
+    Usage:
+        from pyphocorehelpers.Filesystem.generate_code_index import build_code_index
+        project_outputs = build_code_index("C:/Users/pho/repos/Spike3DWorkEnv/NeuroPy/neuropy")
+        project_path, project_db_file_path, tags_file_path, cscope_out_file_path, cscope_files_file_path = project_outputs # unpack
+
 
     """
     project_path = pathlib.Path(project_path)
-    cscope_files = project_path / "cscope.files"
-    cscope_out = project_path / "cscope.out"
-    tags = project_path / "tags"
-    db_path = project_path / "myproject.db"
+    assert project_path.exists()
+    if project_name is None:
+        # infer project name from the path:
+        project_name = project_path.name
+    if details_print:
+        print(f'project_name: {project_name}')
+
+    cscope_files_file_path = project_path / "cscope.files"
+    cscope_out_file_path = project_path / "cscope.out"
+    tags_file_path = project_path / "tags"
+    project_db_file_path = project_path / "myproject.db"
 
     # Find all .py files in the project directory and its subdirectories
     included_py_files = find_py_files(project_path, exclude_dirs=exclude_dirs)
+    if details_print:
+        print(f'\tfound {len(included_py_files)} .py files in project.')
 
     # Write the file paths to cscope.files
-    with open(project_path / "cscope.files", "w") as f:
+    with open(cscope_files_file_path, "w") as f:
         for file_path in included_py_files:
             f.write(str(file_path) + "\n")
 
+    assert cscope_files_file_path.exists()
+
     # Generate the cscope index
-    subprocess.run(["pycscope", "-i", str(cscope_files)], cwd=str(project_path), check=True)
-    
+    subprocess.run(["pycscope", "-i", str(cscope_files_file_path)], cwd=str(project_path), check=True)
+
     # Generate the ctags index
-    subprocess.run(["ctags", "--fields=+i", "-n", "-L", str(cscope_files)], cwd=str(project_path), check=True)
-    
+    subprocess.run(["ctags", "--fields=+i", "-n", "-L", str(cscope_files_file_path)], cwd=str(project_path), check=True)
+    assert cscope_out_file_path.exists()
+
     # Generate the CodeQL database
-    subprocess.run(["cqmakedb", "-s", str(db_path), "-c", str(cscope_out), "-t", str(tags), "-p"], cwd=str(project_path), check=True)
+    subprocess.run(["cqmakedb", "-s", str(project_db_file_path), "-c", str(cscope_out_file_path), "-t", str(tags_file_path), "-p"], cwd=str(project_path), check=True)
+
+    return project_path, project_db_file_path, tags_file_path, cscope_out_file_path, cscope_files_file_path
 
 
 if __name__ == "__main__":

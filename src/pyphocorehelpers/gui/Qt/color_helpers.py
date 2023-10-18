5f3381e639e835d8fb543219f94090f8e7a4297a
Pho Hale <PhoHale@gmail.com>
2023-10-18 16:47:06 -0400

    wrapped color helpers in a ColorFormatConverter class and made @classmethod

--- src/pyphocorehelpers/gui/Qt/color_helpers.py
+++ src/pyphocorehelpers/gui/Qt/color_helpers.py
@@ -1,9 +1,13 @@
-from typing import Dict, List, Optional
+from typing import Dict, List, Optional,  OrderedDict, Union
+from copy import deepcopy
+import numpy as np
+import pandas as pd
+from neuropy.utils.mixins.enum_helpers import StringLiteralComparableEnum
+from pyphocorehelpers.programming_helpers import metadata_attributes
+from pyphocorehelpers.function_helpers import function_attributes
+import pyphoplacecellanalysis.External.pyqtgraph as pg
 from qtpy import QtGui # for QColor
 from qtpy.QtGui import QColor, QBrush, QPen
-
-import numpy as np
-
 
 
 def debug_print_color(color: QColor):
@@ -35,37 +39,86 @@ def build_adjusted_color(color: QColor, hue_shift=0.0, saturation_scale=1.0, val
     return curr_color_copy
 
 
-def hexArgb_to_hexRGBA(hex_Argb_str:str) -> str:
-	""" converts a hexArgb string such as one output by `pen.color().name(QtGui.QColor.HexArgb)` to a regular hex_RGBA string like would be used for matplotlib.
-	
-	'#0b0049ff'
-	
-	QColor.HexArgb: '#AARRGGBB' A “#” character followed by four two-digit hexadecimal numbers (i.e. #AARRGGBB).
-	Output Format (HexRGBA): '#RRGGBBAA'
-	
-	Usage:
-        from pyphocorehelpers.gui.Qt.color_helpers import hexArgb_to_hexRGBA
-		pen=pg.mkPen('#0b0049')
-		hex_Argb_str:str = pen.color().name(QtGui.QColor.HexArgb) # '#ff0b0049'
-		hex_RGBA_str = hexArgb_to_hexRGBA(hex_Argb_str)
-		hex_RGBA_str # '#0b0049ff'
+@metadata_attributes(short_name=None, tags=['color', 'dataseries', 'series', 'helper'], input_requires=[], output_provides=[], uses=[], used_by=[], creation_date='2023-06-21 13:50', related_items=['UnitColoringMode'])
+class ColorFormatConverter:
+     
+    @classmethod
+    def hexArgb_to_hexRGBA(cls, hex_Argb_str:str) -> str:
+        """ converts a hexArgb string such as one output by `pen.color().name(QtGui.QColor.HexArgb)` to a regular hex_RGBA string like would be used for matplotlib.
+        
+        '#0b0049ff'
+        
+        QColor.HexArgb: '#AARRGGBB' A “#” character followed by four two-digit hexadecimal numbers (i.e. #AARRGGBB).
+        Output Format (HexRGBA): '#RRGGBBAA'
+        
+        Usage:
+            from pyphocorehelpers.gui.Qt.color_helpers import hexArgb_to_hexRGBA
+            pen=pg.mkPen('#0b0049')
+            hex_Argb_str:str = pen.color().name(QtGui.QColor.HexArgb) # '#ff0b0049'
+            hex_RGBA_str = hexArgb_to_hexRGBA(hex_Argb_str)
+            hex_RGBA_str # '#0b0049ff'
 
-	"""
-	hex_rgb_str_part = hex_Argb_str[3:] # get the rgb characters
-	hex_alpha_str_part: str = hex_Argb_str[1:3] # get the "alpha" components
-	hex_RGBA_str: str = f"#{hex_rgb_str_part}{hex_alpha_str_part}"
-	return hex_RGBA_str
+        """
+        hex_rgb_str_part = hex_Argb_str[3:] # get the rgb characters
+        hex_alpha_str_part: str = hex_Argb_str[1:3] # get the "alpha" components
+        hex_RGBA_str: str = f"#{hex_rgb_str_part}{hex_alpha_str_part}"
+        return hex_RGBA_str
+
+    @classmethod
+    def qColor_to_hexstring(cls, qcolor: QtGui.QColor, include_alpha:bool=True, use_HexArgb_instead_of_HexRGBA:bool=False) -> str:
+        """ converts a QColor to a hex string 
+        
+        include_alpha: if True, returns a hex string containing the alpha values
+        use_HexArgb_instead_of_HexRGBA:bool; default False, don't use typically.
+            If False results in a string like '#80ff000
+        
+        
+        Notes on getting hex colors:
+            getting the name of a QColor with .name(QtGui.QColor.HexRgb) results in a string like '#ff0000'
+            getting the name of a QColor with .name(QtGui.QColor.HexArgb) results in a string like '#80ff0000'
+        """
+        if not include_alpha:
+            return qcolor.name(QtGui.QColor.HexRgb)
+        else:
+            hex_Argb_str = qcolor.name(QtGui.QColor.HexArgb)
+            if use_HexArgb_instead_of_HexRGBA:
+                return hex_Argb_str
+            else:
+                return cls.hexArgb_to_hexRGBA(hex_Argb_str)
+
+    # ==================================================================================================================== #
+    # Color NDArray Conversions                                                                                             #
+    # ==================================================================================================================== #
+    @classmethod
+    def Colors_NDArray_Convert_to_255_array(cls, colors_ndarray: np.ndarray) -> np.ndarray:
+        """ takes an [4, nCell] np.array of (0.0 - 255.0) values for the color and converts it to a 0.0-1.0 array of the same shape.
+        Reciprocal: Colors_NDArray_Convert_to_zero_to_one_array
+        """
+        converted_colors_ndarray = deepcopy(colors_ndarray)
+        converted_colors_ndarray[0:2, :] *= 255 # [1.0, 0.0, 0.0, 1.0]
+        return converted_colors_ndarray
+    
+    @classmethod
+    def Colors_NDArray_Convert_to_zero_to_one_array(cls, colors_ndarray: np.ndarray) -> np.ndarray:
+        """ takes an [4, nCell] np.array of 0.0-1.0 values for the color and converts it to a (0.0 - 255.0) array of the same shape.
+        Reciprocal: Colors_NDArray_Convert_to_255_array
+        """
+        converted_colors_ndarray = deepcopy(colors_ndarray)
+        converted_colors_ndarray[0:2, :] /= 255
+        return converted_colors_ndarray
+    
 
 
-def convert_pen_brush_to_matplot_kwargs(pen, brush) -> Dict:
-	""" converts a pyqtgraph (pen: QPen, brush: QBrush) combination into matplotlib kwargs dict 
-     Usage:
-        from pyphocorehelpers.gui.Qt.color_helpers import convert_pen_brush_to_matplot_kwargs
+    @classmethod
+    def convert_pen_brush_to_matplot_kwargs(cls, pen, brush) -> Dict:
+        """ converts a pyqtgraph (pen: QPen, brush: QBrush) combination into matplotlib kwargs dict 
+        Usage:
+            from pyphocorehelpers.gui.Qt.color_helpers import convert_pen_brush_to_matplot_kwargs
 
-        matplotlib_rect_kwargs = convert_pen_brush_to_matplot_kwargs(pen, brush)
-        matplotlib_rect_kwargs
+            matplotlib_rect_kwargs = convert_pen_brush_to_matplot_kwargs(pen, brush)
+            matplotlib_rect_kwargs
 
-     """
-	return dict(linewidth=pen.widthF(), edgecolor=hexArgb_to_hexRGBA(pen.color().name(QtGui.QColor.HexArgb)), facecolor=hexArgb_to_hexRGBA(brush.color().name(QtGui.QColor.HexArgb)))
+        """
+        return dict(linewidth=pen.widthF(), edgecolor=cls.hexArgb_to_hexRGBA(pen.color().name(QtGui.QColor.HexArgb)), facecolor=cls.hexArgb_to_hexRGBA(brush.color().name(QtGui.QColor.HexArgb)))
 
 

f1d4e1811090d53b32d949eaef9e743fe61a962d
Pho Hale <PhoHale@gmail.com>
2023-10-23 18:35:35 -0400

    Converted JupyterButtonContainerWidget to an attrs-based class. Generalized functionality

--- src/pyphocorehelpers/gui/Jupyter/JupyterButtonRowWidget.py
+++ src/pyphocorehelpers/gui/Jupyter/JupyterButtonRowWidget.py
@@ -25,7 +25,8 @@ def build_fn_bound_buttons(button_defns, **default_kwargs):
 	return button_dict
 		
 		
-class JupyterButtonRowWidget:
+@define(slots=False)
+class JupyterButtonContainerWidget:
 	""" Displays a clickable row of buttons in the Jupyter Notebook that perform any function 
 	
 	Usage:
@@ -45,26 +46,30 @@ class JupyterButtonRowWidget:
 
 	
 	"""
-	button_list: List
-	root_widget: Optional[widgets.HBox]
+	button_dict: Dict = field(default=Factory(dict))
+	root_widget: Optional[widgets.HBox] = field(default=None)
 
-	def __init__(self, button_defns, defer_display:bool=False):
-		self.button_list = []
-		self.root_widget = None
+	@property
+	def button_list(self) -> List:
+		return list(self.button_dict.values())
+
+
+	@classmethod
+	def init_from_button_defns(cls, button_defns, defer_display:bool=True) -> "JupyterButtonContainerWidget":
 		## Build the widget:
-		self.build_widget(button_defns)
+		_obj = cls()
+		_obj.build_widget(button_defns)
 		# Display if needed
 		if not defer_display:
-			self.display_buttons()
+			_obj.display_buttons()
+		return _obj
+	
 
 	def build_widget(self, button_defns):
 		## builds the buttons from the definitions:
-		self.button_list = []
-		for (a_label, a_fn) in button_defns:
-			a_btn = widgets.Button(description=a_label)
-			a_btn.on_click(a_fn)
-			self.button_list.append(a_btn)
-		self.root_widget = widgets.HBox(self.button_list)
+		self.button_dict = build_fn_bound_buttons(button_defns)
+		raise NotImplementedError
+		# self.root_widget = widgets.HBox(self.button_list)
 		
 
 	def display_buttons(self):
@@ -72,8 +77,43 @@ class JupyterButtonRowWidget:
 		display(self.root_widget)
 
 
+
 @define(slots=False)
-class JupyterButtonColumnWidget:
+class JupyterButtonRowWidget(JupyterButtonContainerWidget):
+	""" Displays a clickable row of buttons in the Jupyter Notebook that perform any function 
+	
+	Usage:
+		from pyphocorehelpers.gui.Jupyter.JupyterButtonRowWidget import JupyterButtonRowWidget
+		from pyphocorehelpers.Filesystem.open_in_system_file_manager import reveal_in_system_file_manager
+
+		# Define the set of buttons:
+		button_defns = [("Output Folder", lambda _: reveal_in_system_file_manager(curr_active_pipeline.get_output_path())),
+				("global pickle", lambda _: reveal_in_system_file_manager(curr_active_pipeline.global_computation_results_pickle_path)),
+				("pipeline pickle", lambda _: reveal_in_system_file_manager(curr_active_pipeline.pickle_path)),
+				(".h5 export", lambda _: reveal_in_system_file_manager(curr_active_pipeline.h5_export_path)),
+				("ViTables .h5 export", lambda _: reveal_in_system_file_manager(curr_active_pipeline.h5_export_path))
+			]
+
+		# Create and display the button
+		button_executor = JupyterButtonRowWidget(button_defns=button_defns)
+
+	
+	"""
+	# button_dict: Dict = field(default=Factory(dict))
+	root_widget: Optional[widgets.HBox] = field(default=None)
+
+	def build_widget(self, button_defns):
+		## builds the buttons from the definitions:
+		self.button_dict = build_fn_bound_buttons(button_defns)
+		self.root_widget = widgets.HBox(self.button_list)
+		
+
+
+
+
+
+@define(slots=False)
+class JupyterButtonColumnWidget(JupyterButtonContainerWidget):
 	""" Displays a clickable column of buttons in the Jupyter Notebook that perform any function 
 	
 	Usage:
@@ -93,24 +133,8 @@ class JupyterButtonColumnWidget:
 
 	
 	"""
-	button_dict: Dict = field(default=Factory(dict))
+	# button_dict: Dict = field(default=Factory(dict))
 	root_widget: Optional[widgets.VBox] = field(default=None)
-
-	@property
-	def button_list(self) -> List:
-		return list(self.button_dict.values())
-
-
-	@classmethod
-	def init_from_button_defns(cls, button_defns, defer_display:bool=False) -> "JupyterButtonColumnWidget":
-		## Build the widget:
-		_obj = cls()
-		_obj.build_widget(button_defns)
-		# Display if needed
-		if not defer_display:
-			_obj.display_buttons()
-		return _obj
-	
 
 	def build_widget(self, button_defns):
 		## builds the buttons from the definitions:
@@ -118,6 +142,6 @@ class JupyterButtonColumnWidget:
 		self.root_widget = widgets.VBox(self.button_list)
 		
 
-	def display_buttons(self):
-		assert self.root_widget is not None
-		display(self.root_widget)
+
+
+

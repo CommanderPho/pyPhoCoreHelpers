8a559757a10820fe3e839ec0bfa385a2cb60d592
Pho Hale <PhoHale@gmail.com>
2023-12-13 02:48:19 -0500

    Unfinished MatplotlibRenderPlotsFigure

--- src/pyphocorehelpers/DataStructure/RenderPlots/MatplotLibRenderPlots.py
+++ src/pyphocorehelpers/DataStructure/RenderPlots/MatplotLibRenderPlots.py
@@ -1,10 +1,14 @@
 from typing import Optional
+import numpy as np
+
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure, FigureBase # FigureBase: both Figure and SubFigure
 from matplotlib.axes import Axes
 
 from pyphocorehelpers.DataStructure.general_parameter_containers import RenderPlots
+from pyphocorehelpers.programming_helpers import metadata_attributes
+from pyphocorehelpers.function_helpers import function_attributes
 
 
 class MatplotlibRenderPlots(RenderPlots):
@@ -49,3 +53,38 @@ class MatplotlibRenderPlots(RenderPlots):
         fig, axes = plt.subplots(*args, **kwargs)
         # check for scalar axes and wrap it in a tuple if needed before setting self.
         return cls(figures=[fig], axes=axes)
+
+
+#TODO 2023-12-13 00:57: - [ ] Custom Figure (TODO)
+
+@metadata_attributes(short_name=None, tags=['not-yet-implemented', 'matplotlib', 'figure'], input_requires=[], output_provides=[], uses=[], used_by=[], creation_date='2023-12-13 00:56', related_items=[])
+class MatplotlibRenderPlotsFigure(Figure):
+    """A figure with emedded data/functions/etc.
+    
+    
+    from pyphocorehelpers.DataStructure.RenderPlots.MatplotLibRenderPlots import MatplotlibRenderPlotsFigure
+    
+    """
+
+    def __init__(self, *args, name='MatplotlibRenderPlots', figures=[], axes=[], context=None, watermark=None, **kwargs):
+        super().__init__(name, *args, figures=figures, axes=axes, context=context, **kwargs) # is this right or do I set them somewhere else?
+
+        # if watermark is not None:
+        #     bbox = dict(boxstyle='square', lw=3, ec='gray',
+        #                 fc=(0.9, 0.9, .9, .5), alpha=0.5)
+        #     self.text(0.5, 0.5, watermark,
+        #               ha='center', va='center', rotation=30,
+        #               fontsize=40, color='gray', alpha=0.5, bbox=bbox)
+
+
+
+"""
+from pyphocorehelpers.DataStructure.RenderPlots.MatplotLibRenderPlots import MatplotlibRenderPlotsFigure
+
+x = np.linspace(-3, 3, 201)
+y = np.tanh(x) + 0.1 * np.cos(5 * x)
+
+plt.figure(FigureClass=MatplotlibRenderPlotsFigure, watermark='draft')
+plt.plot(x, y)
+"""
+

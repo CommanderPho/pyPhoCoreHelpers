29b24f3a3ad17826e3c677ef260567a3da6d26aa
Pho Hale <PhoHale@gmail.com>
2024-08-07 16:33:55 -0400

    cleanup

--- src/pyphocorehelpers/print_helpers.py
+++ src/pyphocorehelpers/print_helpers.py
@@ -1711,42 +1711,24 @@ def _subfn_create_heatmap(data: NDArray, brokenaxes_kwargs=None) -> Optional[Byt
         # fix issues with 1D data like `TypeError: Invalid shape (58,) for image data`
     
     import matplotlib.pyplot as plt
-    from brokenaxes import brokenaxes
+    # from brokenaxes import brokenaxes
     
-    # xmin, xmax, ymin, ymax = (xbin[0], xbin[-1], ybin[0], ybin[-1])
-    # # The extent keyword arguments controls the bounding box in data coordinates that the image will fill specified as (left, right, bottom, top) in data coordinates, the origin keyword argument controls how the image fills that bounding box, and the orientation in the final rendered image is also affected by the axes limits.
-    # extent = (xmin, xmax, ymin, ymax)
-    # # print(f'extent: {extent}')
-    # # extent = None
-    # # We'll also create a black background into which the pixels will fade
-    # # background_black = np.full((*curr_p_x_given_n.shape, 3), 0, dtype=np.uint8)
-
-    imshow_shared_kwargs = {
-        'origin': 'lower',
-        # 'extent': extent,
-    }
-
-    def create_fade_cmap(cmap_name, fade_ratio=0.1):
-        from matplotlib.colors import ListedColormap
-        base = plt.cm.get_cmap(cmap_name)
-        colors = base(np.linspace(0, 1, base.N))
-        alpha = np.linspace(1, 0, int(base.N * fade_ratio))
-        colors[-len(alpha):, -1] = alpha  # Apply the fade out effect to the last fade_ratio of the colormap
-        return ListedColormap(colors)
-
-        
-    active_cmap = 'viridis'
-    # active_cmap = create_fade_cmap('viridis') # fade cmap
+    # # Save the current backend
+    # current_backend = plt.get_backend()
     
-    plt.figure(figsize=(3, 3))
-    # if omission_indices:
-    #     omission_indices = sorted(set(omission_indices))
-    #     breaks = [(omission_indices[i], omission_indices[i+1]-1) for i in range(len(omission_indices)-1)]
-    #     print(f'omission_indices: {omission_indices}, breaks: {breaks}')
-    if brokenaxes_kwargs:
-        print(f'brokenaxes_kwargs: {brokenaxes_kwargs}')
     try:
+        # # Switch to the 'Agg' backend
+        # plt.switch_backend('Agg')
         
+        
+        # xmin, xmax, ymin, ymax = (xbin[0], xbin[-1], ybin[0], ybin[-1])
+        # # The extent keyword arguments controls the bounding box in data coordinates that the image will fill specified as (left, right, bottom, top) in data coordinates, the origin keyword argument controls how the image fills that bounding box, and the orientation in the final rendered image is also affected by the axes limits.
+        # extent = (xmin, xmax, ymin, ymax)
+        # # print(f'extent: {extent}')
+        # # extent = None
+        # # We'll also create a black background into which the pixels will fade
+        # # background_black = np.full((*curr_p_x_given_n.shape, 3), 0, dtype=np.uint8)
+
         imshow_shared_kwargs = {
             'origin': 'lower',
             # 'extent': extent,
@@ -1764,23 +1746,39 @@ def _subfn_create_heatmap(data: NDArray, brokenaxes_kwargs=None) -> Optional[Byt
         active_cmap = 'viridis'
         # active_cmap = create_fade_cmap('viridis') # fade cmap
         
-        if brokenaxes_kwargs:
-            # bax = brokenaxes(xlims=breaks, d=0.015)
-            bax = brokenaxes(**brokenaxes_kwargs, d=0.015)
-            bax.imshow(data, cmap=active_cmap, aspect='auto', **imshow_shared_kwargs)
-            bax.axis('off')
-            bax.draw_diags()
-            # plt.imshow(data, cmap='viridis', 
-            # plt.sca(bax)
-            # Add ellipses at break points
-            # xlims = brokenaxes_kwargs.get('xlims', None)
-            # middle_excluded_range = (xlims[0][-1], xlims[1][0])
-            # start, end = middle_excluded_range
-            # # for start, end in breaks:
-            # mid_point = (start + end) / 2
-            # bax.text(mid_point, data.shape[0] / 2, '...', va='center', ha='center', color='grey', fontsize=9)
         plt.figure(figsize=(3, 3))
+        # if omission_indices:
+        #     omission_indices = sorted(set(omission_indices))
+        #     breaks = [(omission_indices[i], omission_indices[i+1]-1) for i in range(len(omission_indices)-1)]
+        #     print(f'omission_indices: {omission_indices}, breaks: {breaks}')
+        # if brokenaxes_kwargs:
+        #     print(f'brokenaxes_kwargs: {brokenaxes_kwargs}')
             
+
+        #     # plt.imshow(data, cmap=active_cmap)
+        #     # plt.axis('off')
+            
+        #     if brokenaxes_kwargs:
+        #         # bax = brokenaxes(xlims=breaks, d=0.015)
+        #         bax = brokenaxes(**brokenaxes_kwargs, d=0.015)
+        #         bax.imshow(data, cmap=active_cmap, aspect='auto', **imshow_shared_kwargs)
+        #         bax.axis('off')
+        #         bax.draw_diags()
+        #         # plt.imshow(data, cmap='viridis', 
+        #         # plt.sca(bax)
+        #         # Add ellipses at break points
+        #         # xlims = brokenaxes_kwargs.get('xlims', None)
+        #         # middle_excluded_range = (xlims[0][-1], xlims[1][0])
+        #         # start, end = middle_excluded_range
+        #         # # for start, end in breaks:
+        #         # mid_point = (start + end) / 2
+        #         # bax.text(mid_point, data.shape[0] / 2, '...', va='center', ha='center', color='grey', fontsize=9)
+                
+            # else:
+            #     plt.imshow(data, cmap=active_cmap, **imshow_shared_kwargs)
+            #     plt.axis('off')
+            #     pass
+        # else:
         plt.imshow(data, cmap=active_cmap, **imshow_shared_kwargs)
         plt.axis('off')
             

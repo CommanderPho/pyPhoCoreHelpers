c56f4c7661c8fa9b059b86959c722be3aeafc3e0
Pho Hale <PhoHale@gmail.com>
2024-08-08 08:08:23 -0400

    cleanup and optimize

--- src/pyphocorehelpers/ipython_helpers.py
+++ src/pyphocorehelpers/ipython_helpers.py
@@ -1,13 +1,52 @@
+from typing import Dict, List, Tuple, Optional, Callable, Union, Any
 from IPython.core.magic import Magics, magics_class, cell_magic, line_magic
 from IPython import get_ipython
 from IPython.display import display
 import numpy as np
 
+
+def _parse_ndarray_preview_params(line: str) -> Dict:
+    """ 
+    %%ndarray_preview height=500, width=200, include_plaintext_repr=False
+    
+    %%ndarray_preview height=None, width=100, include_plaintext_repr=True, include_shape=False, horizontal_layout=False
+    
+    """
+    # Split the magic line by commas to get individual key-value pairs (like `%%ndarray_preview height=500, width=200, include_plaintext_repr=False`)
+    params = line.split(',')
+    config = {}
+    
+    for param in params:
+        key, value = param.split('=')
+        key = key.strip()
+        value = value.strip()
+        # Convert string representation to appropriate type
+        if value.lower() in ['true', 'false']:
+            value = value.lower() == 'true'
+        elif value.isdigit():
+            value = int(value)
+        elif value.lower() =='none':
+            value = None
+        else:
+            try:
+                value = float(value)
+            except ValueError:
+                pass
+        config[key] = value
+        
+    return config
+
+
 @magics_class
 class MyMagics(Magics):
     """ 
-    from pyphocorehelpers.ipython_helpers import MyMagics
-    
+    ## Usage:
+
+        from pyphocorehelpers.ipython_helpers import MyMagics
+        # Register the magic
+        ip = get_ipython()
+        ip.register_magics(MyMagics)
+
     """
     @cell_magic
     def setvar(self, line, cell):
@@ -21,30 +60,18 @@ class MyMagics(Magics):
         
     @line_magic
     def config_ndarray_preview(self, line):
-
-        """ Allows the user to change the thumbnail image size like:
+        """ Updates the display preview config (persistantly). Is a line magic because it updates the ipython display_formatter directly and will persist for the remainder (and the following) cells
+            Allows the user to change the thumbnail image size like:
     
         # call the line magic
         %config_ndarray_preview width=500 
         
-        
         """
         from pyphocorehelpers.preferences_helpers import array_repr_with_graphical_preview
-        key, value = line.split('=')
-        key = key.strip()
-        value = value.strip()
-
-        width = None
-        height = None
-        if key == "width":
-            width = int(value)
-        elif key == "height":
-            height = int(value)
-        else:
-            raise KeyError(f"Unknown configuration key: {key}")
+        config = _parse_ndarray_preview_params(line=line)
 
         ip = get_ipython()
-        array_repr_with_graphical_preview(ip=ip, width=width, height=height)
+        array_repr_with_graphical_preview(ip=ip, **config)
         
 
     @cell_magic
@@ -56,51 +83,22 @@ class MyMagics(Magics):
         
         """
         from pyphocorehelpers.preferences_helpers import array_repr_with_graphical_preview
-        from pyphocorehelpers.print_helpers import array_preview_with_heatmap_repr_html
             
-        debug_print = True
+        debug_print = False
 
         # Split the magic line by commas to get individual key-value pairs (like `%%ndarray_preview height=500, width=200, include_plaintext_repr=False`)
         params = line.split(',')
-        config = {}
+        config = _parse_ndarray_preview_params(line=line) 
         
-        for param in params:
-            key, value = param.split('=')
-            key = key.strip()
-            value = value.strip()
-            # Convert string representation to appropriate type
-            if value.lower() in ['true', 'false']:
-                value = value.lower() == 'true'
-            elif value.isdigit():
-                value = int(value)
-            elif value.lower() =='none':
-                value = None
-            else:
-                try:
-                    value = float(value)
-                except ValueError:
-                    pass
-            config[key] = value
-            
         if debug_print:
             print(f'config: {config}\n')
-            
-        # integer_keys = ['width', 'height']
-        # for k in integer_keys:
-        #     if k in config:
-        #         if config[k] is not None:
-        #             config[k] = int(config[k]) # convert to integer
-        
             
         ip = get_ipython()
         
         ## Backup the current NDArray formatter
         _bak_formatter = ip.display_formatter.formatters['text/html'].type_printers.pop(np.ndarray, None)
-        # if _bak_formatter is not None:
-        #     print(_bak_formatter)
             
         # Register the custom display function for NumPy arrays for the duration of the cell:
-        # ip.display_formatter.formatters['text/html'].for_type(np.ndarray, lambda arr: array_preview_with_heatmap_repr_html(arr, **config))
         array_repr_with_graphical_preview(ip=ip, **config)
         
         # Execute the cell content and capture the output
@@ -112,12 +110,6 @@ class MyMagics(Magics):
         # Fetch the variables created in the cell for display
         # output = {var_name: self.shell.user_ns[var_name] for var_name in self.shell.user_ns if isinstance(self.shell.user_ns[var_name], np.ndarray)}
         display(output)
-
-        # for var in output.values():
-        #     display(var)
-
-        # display(output)
-        # return output
         
         # Remove the custom formatter
         ip.display_formatter.formatters['text/html'].type_printers.pop(np.ndarray, None)
@@ -130,7 +122,4 @@ class MyMagics(Magics):
         # return output
     
 
-# ## Usage:
-# # Register the magic
-# ip = get_ipython()
-# ip.register_magics(MyMagics)
+

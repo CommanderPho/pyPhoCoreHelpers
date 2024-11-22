e3b3979c0baac800fbf7127a26276f091374cf83
Pho Hale <PhoHale@gmail.com>
2024-11-22 09:26:54 -0500

    multi_checkbox_widget

--- src/pyphocorehelpers/gui/Jupyter/simple_widgets.py
+++ src/pyphocorehelpers/gui/Jupyter/simple_widgets.py
@@ -413,3 +413,78 @@ def create_tab_widget(display_dict: Dict[str, Any], **tab_kwargs) -> widgets.Tab
 
 
 
+# ==================================================================================================================== #
+# Selection Widgets                                                                                                    #
+# ==================================================================================================================== #
+
+# @function_attributes(short_name=None, tags=['widget', 'jupyter', 'ipywidgets'], input_requires=[], output_provides=[], uses=[], used_by=[], creation_date='2024-11-22 09:14', related_items=[])
+def multi_checkbox_widget(options_dict):
+    """ Widget with a search field and lots of checkboxes
+    
+    import ipywidgets as widgets
+    from pyphocorehelpers.gui.Jupyter.simple_widgets import multi_checkbox_widget
+
+    def f(**kwargs):
+        print(kwargs)
+
+    options_dict = {
+        x: widgets.Checkbox(
+            description=x, 
+            value=False,
+            style={"description_width":"0px"}
+        ) for x in ['hello','world']
+    }
+    ui = multi_checkbox_widget(options_dict)
+    out = widgets.interactive_output(f, options_dict)
+    display(widgets.HBox([ui, out]))
+
+    """
+    search_widget = widgets.Text()
+    output_widget = widgets.Output()
+    options = [x for x in options_dict.values()]
+    options_layout = widgets.Layout(
+        overflow='auto',
+        border='1px solid black',
+        width='300px',
+        height='300px',
+        flex_flow='column',
+        display='flex'
+    )
+    
+    #selected_widget = wid.Box(children=[options[0]])
+    options_widget = widgets.VBox(options, layout=options_layout)
+    #left_widget = wid.VBox(search_widget, selected_widget)
+    multi_select = widgets.VBox([search_widget, options_widget])
+
+    @output_widget.capture()
+    def on_checkbox_change(change):
+        
+        selected_recipe = change["owner"].description
+        #print(options_widget.children)
+        #selected_item = wid.Button(description = change["new"])
+        #selected_widget.children = [] #selected_widget.children + [selected_item]
+        options_widget.children = sorted([x for x in options_widget.children], key = lambda x: x.value, reverse = True)
+        
+    for checkbox in options:
+        checkbox.observe(on_checkbox_change, names="value")
+
+    # Wire the search field to the checkboxes
+    @output_widget.capture()
+    def on_text_change(change):
+        search_input = change['new']
+        if search_input == '':
+            # Reset search field
+            new_options = sorted(options, key = lambda x: x.value, reverse = True)
+        else:
+            # Filter by search field using difflib.
+            #close_matches = difflib.get_close_matches(search_input, list(options_dict.keys()), cutoff=0.0)
+            close_matches = [x for x in list(options_dict.keys()) if str.lower(search_input.strip('')) in str.lower(x)]
+            new_options = sorted(
+                [x for x in options if x.description in close_matches], 
+                key = lambda x: x.value, reverse = True
+            ) #[options_dict[x] for x in close_matches]
+        options_widget.children = new_options
+
+    search_widget.observe(on_text_change, names='value')
+    display(output_widget)
+    return multi_select

c82d339ba5b08ac98bb029d3fb60e4f4e30b3778
Pho Hale <PhoHale@gmail.com>
2025-06-27 02:51:41 -0400

    fixed `__or__` for CascadingDynamicPlotsList

--- src/pyphocorehelpers/gui/PyVista/CascadingDynamicPlotsList.py
+++ src/pyphocorehelpers/gui/PyVista/CascadingDynamicPlotsList.py
@@ -34,9 +34,13 @@ class CascadingDynamicPlotsList(DynamicParameters):
         curr_plot_actors = self.plotActors
         output_results = []
         for curr_plot_actor in curr_plot_actors:
-            curr_output = getattr(curr_plot_actor, child_method_name)(*args, **kwargs)
-            # curr_output = CascadingDynamicPlotsList.apply_on_all(curr_plot_actor, child_method_name, *args, **kwargs)
-            # curr_output = functionToApply(curr_plot_actor, *args, **kwargs)
+            if curr_plot_actor is not None:
+                curr_output = getattr(curr_plot_actor, child_method_name)(*args, **kwargs)
+                # curr_output = CascadingDynamicPlotsList.apply_on_all(curr_plot_actor, child_method_name, *args, **kwargs)
+                # curr_output = functionToApply(curr_plot_actor, *args, **kwargs)
+            else:
+                ## is None
+                curr_output = None
             output_results.append(curr_output)
         return output_results
     
@@ -53,5 +57,38 @@ class CascadingDynamicPlotsList(DynamicParameters):
         for obj in seq:
             getattr(obj, method)(*args, **kwargs)
             
+
+
+    def __or__(self, other):
+        """ Used with vertical bar operator: |
+        
+        Usage:
+            (_test_complete_spike_analysis_config | _test_partial_spike_analysis_config)    
+        """
+        if isinstance(other, (dict)):
+            other_dict = other
+        elif isinstance(other, (CascadingDynamicPlotsList, DynamicParameters)):
+            other_dict = other.to_dict()
+        else:
+            # try to convert the other type into a dict using all known available methods: DynamicContainer
+            try:
+                other_dict = other.to_dict() # try to convert to dict using the .to_dict() method if possible
+            except Exception as e:
+                # If that failed, fallback to trying to access the object's .__dict__ property
+                try:
+                    other_dict = dict(other.items())
+                except Exception as e:
+                    # Give up, can't convert!                
+                    print(f'UNHANDLED TYPE: type(other): {type(other)}, other: {other}')
+                    # raise NotImplementedError            
+                    other_dict = None
+                    raise e
+
+                pass # other_dict               
+        
+        dict_or = self.to_dict().__or__(other_dict)
+        return CascadingDynamicPlotsList.init_from_dict(dict_or)
+    
+
 
 # ds
